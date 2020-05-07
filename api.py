e25c93509c652d60b2dd5e0c54632f980df523be
Crawford Collins <crawford.collins@gmail.com>
2020-05-06 19:29:35 -0500

    nearly complete need to figure out redirect to search with the search-term

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import json
 
 from falcon_jinja2 import FalconTemplate
 
-
+labels = ["positive", "negative", "undecidable"]
 falcon_template = FalconTemplate()
 app = falcon.API()
 
@@ -12,7 +12,15 @@ app = falcon.API()
 class Home(object):
     @falcon_template.render("index.html")
     def on_get(self, req, resp):
-        resp.context = {"framework": "Falcon"}
+        with sqlite3.connect("hand-labeled-data.db") as conn:
+            dict_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+            conn.row_factory = dict_factory
+            query = (
+                "SELECT rating, COUNT(rating) as num FROM labeled_data group by rating"
+            )
+            result = conn.execute(query).fetchall()
+        resp.status = falcon.HTTP_200
+        resp.context = {"labeled_data": list(result)}
 
 
 home = Home()
@@ -21,9 +29,9 @@ app.add_route("/", home)
 
 class Search(object):
     @falcon_template.render("search_result.html")
-    def on_post(self, req, resp):
-        resp.context = {}
-        search_term = req.stream.read().decode("UTF-8").split("=")[1]
+    def on_post(self, req, resp, search_term=None):
+        if search_term is None:
+            search_term = req.stream.read().decode("UTF-8").split("=")[1]
         resp.context["search_term"] = search_term
         resp.status = falcon.HTTP_200
         with sqlite3.connect("hand-labeled-data.db") as conn:
@@ -33,22 +41,34 @@ class Search(object):
                 "SELECT text,rowid,rating FROM data WHERE data MATCH 'terrible' LIMIT 1"
             )
             search_result = conn.execute(query).fetchone()
-            resp.context["search_result"] = search_result
+        resp.context["search_result"] = search_result
+        resp.context["labels"] = labels
 
 
 search = Search()
 app.add_route("/search", search)
+app.add_route("/search/{search_term}", search)
 
 
 class SubmitRating(object):
-    def on_post(self, req, resp, rowid):
-        resp.status = falcon.HTTP_200
+    def on_post(self, req, resp, rowid, search_term):
+        resp.status = falcon.HTTP_302
         req_dict = byte_stream_to_dict(req)
         resp.body = json.dumps(req_dict)
+        with sqlite3.connect("hand-labeled-data.db") as conn:
+            dict_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+            conn.row_factory = dict_factory
+            extract = "SELECT text FROM data WHERE rowid = (?)"
+            data = conn.execute(extract, (int(rowid),)).fetchone()
+            insert = "INSERT INTO labeled_data VALUES (?,?,?)"
+            conn.execute(insert, (rowid, data["text"], req_dict["label"]))
+            delete = "DELETE FROM data WHERE rowid = (?)"
+            conn.execute(delete, (int(rowid),))
+        resp.location = f"/search/{search_term}"
 
 
 submit = SubmitRating()
-app.add_route("/submit_rating/{rowid:int}", submit)
+app.add_route("/submit_rating/{rowid:int}/{search_term}", submit)
 
 
 def byte_stream_to_dict(req):
