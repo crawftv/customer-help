a53a5e59a78ca2a8830a5495f532966680218bd5
crawford <crawford.collins@gmail.com>
2020-05-07 17:25:37 -0500

    app is working

--- api.py
+++ api.py
@@ -38,11 +38,26 @@ class Search(object):
             dict_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
             conn.row_factory = dict_factory
             query = (
-                "SELECT text,rowid,rating FROM data WHERE data MATCH 'terrible' LIMIT 1"
+                "SELECT text,rowid,rating FROM data WHERE data MATCH (?) LIMIT 1"
             )
-            search_result = conn.execute(query).fetchone()
+            search_result = conn.execute(query,(search_term,)).fetchone()
         resp.context["search_result"] = search_result
         resp.context["labels"] = labels
+    
+    @falcon_template.render("search_result.html")
+    def on_get(self,req,resp,search_term=None):
+        with sqlite3.connect("hand-labeled-data.db") as conn:
+            dict_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+            conn.row_factory = dict_factory
+            query = (
+                "SELECT text,rowid,rating FROM data WHERE data MATCH (?) LIMIT 1"
+            )
+            search_result = conn.execute(query,(search_term,)).fetchone()
+        resp.context["search_term"] = search_term
+        resp.context["search_result"] = search_result
+        resp.context["labels"] = labels
+        
+
 
 
 search = Search()
